f2200b645e5f8f4e6bc784c25efa08d85c87bf33
Václav <xpastu02@vutbr.cz>
2020-09-23 15:48:01 +0200

    Done

--- proj1/GccApplication1/Debug/main.d
+++ proj1/GccApplication1/Debug/main.d
@@ -12,7 +12,9 @@ main.d main.o: .././main.c \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
 
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
@@ -41,3 +43,7 @@ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- proj1/GccApplication1/main.c
+++ proj1/GccApplication1/main.c
@@ -11,7 +11,10 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define SHORT_DELAY 500      // Delay in miliseconds
+#define SHORT_D 200     // Delay in miliseconds
+#define LONG_D SHORT_D*3 // Delay in miliseconds
+
+
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay func
 #endif
@@ -19,12 +22,47 @@
 /* Includes ----------------------------------------------------------*/
 #include <util/delay.h>     // Functions for busy-wait delay loops
 #include <avr/io.h>         // AVR device-specific IO definitions
+//#include <stdio.h>
+#include <string.h>
 
 /* Variables ---------------------------------------------------------*/
 
 /* Function prototypes -----------------------------------------------*/
+void blink(char);
+void morse_char(char);
+void morse_code(char *);
 
 /* Functions ---------------------------------------------------------*/
+void blink(char charD){
+	PORTB = PORTB ^ (1<<LED_GREEN);
+	if(charD == 'S')
+		_delay_ms(SHORT_D); // tecka
+	else
+		_delay_ms(LONG_D); // carka	
+		
+	PORTB = PORTB ^ (1<<LED_GREEN);
+	_delay_ms(SHORT_D);
+	return;
+}
+
+void morse_char(char character){
+	switch(character){
+		case 'D': blink('L'); blink('S'); blink('S'); break;
+		case 'E': blink('S'); break;
+		case '2': blink('S'); blink('S'); blink('L'); blink('L'); blink('L'); break;
+	}
+	_delay_ms(LONG_D);
+	return;
+}
+
+void morse_code(char *name){
+	for(int i = 0; i < strlen(name); i++)
+		morse_char(name[i]);
+	return;
+}
+
+
+
 /**
  * Toggle one LED and use the function from the delay library.
  */
@@ -37,18 +75,15 @@ int main(void)
     // Set pin LOW in Data Register (LED off)
     // PORTB = PORTB and 1101 1111
     PORTB = PORTB & ~(1<<LED_GREEN);
-
+	
+	char *name = "DE2";
     // Infinite loop
     while (1)
     {
-        // Pause several milliseconds
-        _delay_ms(SHORT_DELAY);
-
-        // Invert LED in Data Register
-        // PORTB = PORTB xor 0010 0000
-        PORTB = PORTB ^ (1<<LED_GREEN);
+		morse_code(name);
+		for(int i = 0; i < 7; i++)
+			_delay_ms(SHORT_D);
     }
-
     // Will never reach this
     return 0;
 }
